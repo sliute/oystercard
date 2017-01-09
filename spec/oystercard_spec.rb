ac5bf45961e3812220e6768117bf0e6209408672
Stefan Liute <stefanliute@gmail.com>
2017-01-09 15:48:19 +0000

    Challenge 2.2 (6)

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,14 @@ describe Oystercard do
     expect(oystercard.balance).to eq 0
   end
 
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'tops up the balance with given amount' do
+      # oystercard.top_up(33)
+      # expect(oystercard.balance).to eq 33
+      expect{ oystercard.top_up 33 }.to change{ subject.balance }.by 33
+    end
+  end
+
 end
