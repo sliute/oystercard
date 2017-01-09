298ee81e13860dbf3fbc277235dbec5ca9654976
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-01-09 15:30:22 +0000

    Challenge 2.1

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
+  subject(:oystercard) {described_class.new }
+
+  it 'newly initialized cards have a balance of 0' do
+    expect(oystercard.balance).to eq 0
+  end
 
 end
